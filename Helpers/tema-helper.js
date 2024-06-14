4cf0c7984648e78264630da493eb6437adfa4046
Brunomv06 magalhães <brunomv0610@gmail.com>
2024-06-14 19:44:00 +0000

    pronto arrumei

--- Helpers/tema-helper.js
+++ Helpers/tema-helper.js
@@ -1,28 +1,58 @@
-let tema = "claro"
 
-export function trocarTema(body, botaoTema){
-    if (localStorage.getItem("tema")){
-        tema = localStorage.getItema("tema")
-    }
-    
-    if (tema === "claro"){
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-        botaoTema.style.justifyContent = "flex-end"
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-        botaoTema.style.justifyContent = "flex-start"
-    }
-}
+let tema = "claro";
 
-export function verificarTema(body, botaoTema){
+export function trocarTema(body, botaoTema) {
     if(localStorage.getItem("tema")){
         tema = localStorage.getItem("tema")
     }
 
-    if (tema === "escuro"){
+
+    if(tema === "claro") {
         body.classList.add("escuro")
-        botaoTema.style.justifyContent = "flex-end"   
+        localStorage.setItem("tema", "escuro")
+        botaoTema.style.justifyContent = 'flex-end'
+    } else {
+        body.classList.remove("escuro")
+        localStorage.setItem("tema", "claro")
+        botaoTema.style.justifyContent = 'flex-start'
     }
 }
+
+export function verificarTema(body, botaoTema) {
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+
+    if ( tema === "escuro" ){
+        body.classList.add("escuro")
+        botaoTema.style.justifyContent = 'flex-end'
+    }
+}
+
+
+ 
+
+
+
+
+    
+    
+
+
+     
+
+      
+
+
+
+
+ 
+
+
+ 
+        
+       
+
+      
+     
+  
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import { trocarTema, verificarTema } from "./Helpers/tema-helper.js"
 
+
+import { trocarTema, verificarTema } from './helpers/tema-helper.js'
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
@@ -14,11 +15,28 @@ const botoesAssunto = document.querySelectorAll(".assuntos button")
 
 botoesAssunto.forEach(botao => {
     botao.addEventListener("click", selecionarAssunto)
+
+    function selecionarAssunto(evento){
+        const classeBotao = evento.target.className
+        const assunto = document.querySelector(`.${classeBotao} span`).innerText
+        localStorage.setItem("assunto", assunto)
+        window.location.href = "./pages/quiz/quiz.html"
+    }
 })
 
-function selecionarAssunto(evento){
-    const classeBotao = evento.target.className
-    const assuntos = evento.target.querySelector(`.${classeBotao} span`).innerText
-    localStorage.setItem("assunto", assunto)
-    window.location.href = "./pages/quiz/quiz.html"
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
